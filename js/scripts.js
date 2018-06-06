d555679a48dace73d24e32059e5fe4c8ff0beae9
Matt Staudacher and Steven Colburn <matt@>
2018-06-06 14:43:49 -0700

    optomized for qu and y exceptions

--- js/scripts.js
+++ js/scripts.js
@@ -20,24 +20,22 @@ function translatePigLatin(original)
   {
     var word = originWords[index];
     var firstCharacter = word.charAt(0).toString();
-    //console.log("Character: "+firstCharacter);
-    if(!isNaN(parseInt(firstCharacter)) || firstCharacter==="")
-    {
-      //Must be a number or empty string
-      //Don't Change anything
-      console.log("Number Branch: " + word);
-    }
-    else if(isVowel(firstCharacter))
+
+    console.log("Character: "+firstCharacter);
+    if(isVowel(firstCharacter))
     {
       //Beginning character of word is a vowel run function stepVowel
       console.log("Vowel Branch: " + word);
       word = stepVowel(word);
     }
-    else
+    else if(isConsonent(firstCharacter)||isY(firstCharacter))
     {
       //Beginning character of word is a consonant run function stepConsonant
       console.log("Consonant Branch: " + word);
       word = stepConsonant(word);
+    }
+    else {
+
     }
     originWords[index] = word;
   }
@@ -48,28 +46,35 @@ function translatePigLatin(original)
 function stepConsonant(word)
 {
   var compareWord = word.toLowerCase();
-  var saveChar = "";
+  var specialFound = false;
+  var savedChar = "";
+  var savedLoc = 0;
 
-  if(compareWord.charAt(0)==="q" && compareWord.charAt(1)==="u")
+  if(compareWord.charAt(0)==="q" &&  compareWord.charAt(1)==="u")
   {
-    saveChar = word.charAt(1);
-    var tempWords = word.split("");
-    //replace u with a character that isn't a vowel
-    tempWords[1] = "b";
-    word = tempWords.join("");
+    specialFound = true;
+    word = replaceAt(word,1,"b");
+    savedChar = "u";
+    savedLoc = 1;
   }
+  else if(compareWord.charAt(0)==="y")
+  {
+    specialFound = true;
+    word = replaceAt(word,0,"b");
+    savedChar = "y";
+    savedLoc = 0;
+  }
+
+
 
   var vowel = findSeperationVowel(word);
   var modifiedWords = word.split(vowel);
 
   var shiftCharacters = modifiedWords.shift();
   //replace the b with a u if saveChar is not blank
-  if(saveChar != "")
+  if(specialFound)
   {
-    var tempWords = shiftCharacters.split("");
-    //replace b with u.
-    tempWords[1] = "u";
-    shiftCharacters = tempWords.join("");
+    shiftCharacters = replaceAt(shiftCharacters,savedLoc,savedChar);
   }
 
   modifiedWords.unshift(" ");
@@ -78,6 +83,14 @@ function stepConsonant(word)
   modifiedWords.push(tempString);
   var modifiedWord = modifiedWords.join(vowel);
   return modifiedWord;
+}
+
+function replaceAt(word,point,replaceWith)
+{
+  var tempWords = word.split("");
+  //replace u with a character that isn't a vowel
+  tempWords[point] = replaceWith;
+  return (tempWords.join(""));
 }
 
 
@@ -116,4 +129,24 @@ function isVowel(character)
     }
   });
   return boolIsVowel;
+}
+
+function isConsonent(character)
+{
+  var consonents = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x"];
+  character = character.toLowerCase();
+  var boolIsConsonent = false;
+  consonents.forEach(function(consonent){
+    if(character === consonent)
+    {
+      boolIsConsonent = true;
+    }
+  });
+  return boolIsConsonent;
+}
+
+function isY(character)
+{
+  character = character.toLowerCase();
+  return (character==="y");
 }
