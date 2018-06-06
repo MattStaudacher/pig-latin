a455daa8b3121113263a2c5824efa71ac447bd63
Matt Staudacher and Steven Colburn <matt@>
2018-06-06 10:11:31 -0700

    check first letter is vowel

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,38 @@ $(function(){
   $("#form-one").submit(function(event){
     event.preventDefault();
     var original = $("#input").val();
-    outputAnswer(original);
-
-
+    outputAnswer(translatePigLatin(original));
   });
 });
 
 function outputAnswer(tempString)
 {
   $("#output").text(tempString);
+}
+
+function translatePigLatin(original)
+{
+  originlength = original.length;
+  //return original.charAt(0);
+  if(isVowel(original.charAt(0)))
+  {
+    return "vowel";
+  }
+  else {
+    return original;
+  }
+
+}
+
+function isVowel(character)
+{
+  character = character.toLowerCase();
+  var bool = (compareTo(character,"a")  || compareTo(character,"e") || compareTo(character,"i") || compareTo(character,"o") || compareTo(character,"u"));
+  return bool;
+
+}
+
+function compareTo(one,two)
+{
+  return (one===two);
 }
